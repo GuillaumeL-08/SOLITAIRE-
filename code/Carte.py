b06c123a6bb372dea50c765dc95003aa0741e97b
Kazuto Hikigaya <152779934+Kelvyn3000@users.noreply.github.com>
2025-10-18 23:12:50 +0200

    Update Carte.py

--- code/Carte.py
+++ code/Carte.py
@@ -1,8 +1,10 @@
 class Carte:
     def __init__(self, couleur, valeur, visible=False) -> None:
+        "initialisation des objet"
         self.couleur = couleur
         self.valeur = valeur
         self.visible = visible
 
     def __repr__(self) -> str :
+        "appelle dans la console"
         return f"{self.valeur}{self.couleur}" if self.visible else "XX"
