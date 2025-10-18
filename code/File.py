ce69fa41165e19785a06142f32d14d8e753a6c66
Kazuto Hikigaya <152779934+Kelvyn3000@users.noreply.github.com>
2025-10-18 23:15:15 +0200

    Update File.py

--- code/File.py
+++ code/File.py
@@ -2,13 +2,13 @@ class File:
     def __init__(self):
         self.p = []
 
-    def est_vide(self):
+    def est_vide(self)  -> list:
         return len(self.p) == 0
 
-    def enfiler(self, elem):
+    def enfiler(self, elem) -> None:
         self.p.append(elem)
 
-    def tete(self):
+    def tete(self) :
         return self.p[0]
 
     def defiler(self):
@@ -16,5 +16,6 @@ class File:
             return self.p.pop(0)
         return None
 
-    def taille(self):
+    def taille(self) -> int:
+        "retourne la taille de la file"
         return len(self.p)
