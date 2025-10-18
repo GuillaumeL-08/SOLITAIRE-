421c9746300509a1440c41c9eaf2c9ee61689d35
Kazuto Hikigaya <152779934+Kelvyn3000@users.noreply.github.com>
2025-10-18 23:11:59 +0200

    Update Carte.py

--- code/Carte.py
+++ code/Carte.py
@@ -1,8 +1,8 @@
 class Carte:
-    def __init__(self, couleur, valeur, visible=False):
+    def __init__(self, couleur, valeur, visible=False) -> None:
         self.couleur = couleur
         self.valeur = valeur
         self.visible = visible
 
-    def __repr__(self):
+    def __repr__(self) -> str :
         return f"{self.valeur}{self.couleur}" if self.visible else "XX"
