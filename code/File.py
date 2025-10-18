23ed61718412fb61dc74f63c5cd97ac5e9202964
Kazuto Hikigaya <152779934+Kelvyn3000@users.noreply.github.com>
2025-10-18 23:18:51 +0200

    Update File.py

--- code/File.py
+++ code/File.py
@@ -1,17 +1,25 @@
 class File:
-    def __init__(self):
+    def __init__(self) -> None:
+        "initialise les attributs"
         self.p = []
 
-    def est_vide(self)  -> list:
-        return len(self.p) == 0
+    def est_vide(self) -> bool:
+        "renvoie True si la file est vide sinon False"
+        if self.p == []:
+            return True
+        else:
+            return False
 
     def enfiler(self, elem) -> None:
+        "ajoute un elemnt à la file"
         self.p.append(elem)
 
-    def tete(self) :
+    def tete(self)  :
+        "renvoie le premier élément de la file"
         return self.p[0]
 
     def defiler(self):
+        "renvoie le premier élément de la file puis le supprime de la file"
         if not self.est_vide():
             return self.p.pop(0)
         return None
