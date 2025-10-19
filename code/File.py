0cb4faae69e6351d00c1e5ec0a789ebcc996d273
Yochy09 <merlinpowerroch@gmail.com>
2025-10-20 01:07:29 +0200

    Add comments to methods in File class

--- code/File.py
+++ code/File.py
@@ -1,29 +1,24 @@
 class File:
-    def __init__(self) -> None:
-        "initialise les attributs"
+    def __init__(self) -> None: #initialise les attributs
         self.p = []
 
-    def est_vide(self) -> bool:
-        "renvoie True si la file est vide sinon False"
+    def est_vide(self) -> bool:  #renvoie True si la file est vide sinon False
         if self.p == []:
             return True
         else:
             return False
 
-    def enfiler(self, elem) -> None:
-        "ajoute un elemnt à la file"
+    def enfiler(self, elem) -> None: #ajoute un elemnt à la file
         self.p.append(elem)
 
-    def tete(self)  :
-        "renvoie le premier élément de la file"
+    def tete(self)  : # renvoie le premier élément de la file
         return self.p[0]
 
-    def defiler(self):
-        "renvoie le premier élément de la file puis le supprime de la file"
+    def defiler(self): # renvoie le premier élément de la file puis le supprime de la file
         if not self.est_vide():
             return self.p.pop(0)
         return None
 
-    def taille(self) -> int:
-        "retourne la taille de la file"
+    def taille(self) -> int: # renvoie la taille de la file
         return len(self.p)
+
