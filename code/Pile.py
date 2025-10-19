5ef04fa7d479fc6961a5ff55ca21e135b0a12093
Yochy09 <merlinpowerroch@gmail.com>
2025-10-20 01:04:41 +0200

    Update Pile.py

--- code/Pile.py
+++ code/Pile.py
@@ -1,22 +1,23 @@
 class Pile:
-    def __init__(self, pile=None):
+    def __init__(self, pile=None):  # Initialisation de la pile
         self.pile = pile if pile else []
 
-    def est_vide(self):
+    def est_vide(self):  # Vérifie si la pile est vide
         return len(self.pile) == 0
 
-    def empiler(self, element):
+    def empiler(self, element): # Ajoute un élément au sommet de la pile
         self.pile.append(element)
 
     def depiler(self):
-        if not self.est_vide():
+        if not self.est_vide(): # Retire et retourne l'élément au sommet de la pile
             return self.pile.pop()
         return None
 
-    def sommet(self):
+    def sommet(self): # Retourne l'élément au sommet de la pile sans le retirer
         if not self.est_vide():
             return self.pile[-1]
         return None
 
-    def taille(self):
+    def taille(self): # Retourne le nombre d'éléments dans la pile
         return len(self.pile)
+
